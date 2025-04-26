cbe560ac1fb7ab8a0c8bcb01374d8acb40f36a0a
Danah <danah.kim@kakao.com>
2025-04-27 06:31:51 +0900

    chore: svg 설정 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+declare module '*.svg' {
+  import type * as React from 'react';
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export = ReactComponent;
+}
