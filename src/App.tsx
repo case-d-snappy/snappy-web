fa7a670233cc36d592b589cec1af187672412792
Danah <danah.kim@kakao.com>
2025-04-29 23:04:50 +0900

    feat: 푸터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { GlobalPortal } from 'utils/GlobalPortal';
 const Concept = lazy(() => import('components/Concept'));
 const Strengths = lazy(() => import('components/Strengths'));
 const Subscription = lazy(() => import('components/Subscription'));
+const Footer = lazy(() => import('components/Footer'));
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Concept />
         <Strengths />
         <Subscription />
+        <Footer />
       </main>
       <ScrollHint />
     </GlobalPortal.Provider>
